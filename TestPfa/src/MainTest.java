bb8407dfd67b9012f95214ac91094dfc0be6bf21
Hniss <nissoul.hamza@gmail.com>
2021-03-02 22:09:34 +0100

    je me suis perturber

--- TestPfa/src/MainTest.java
+++ TestPfa/src/MainTest.java
@@ -4,6 +4,11 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is a class test");
+		//know i'm adding someting for seeing modif
+		int a = 1;
+		int b = 1;
+		int c = a + b ;
+		System.out.println("this is the sum of a + b : "+c);
 
 	}
 
